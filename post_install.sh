4712606fe11281a18dd682c178ad14ee3180efc6
laportad <64189396+laportad@users.noreply.github.com>
2020-07-17 08:11:47 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -14,6 +14,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# The script configures the Slurm cluster after the deployment. Replace <bucket> with your bucket name.
+
 . "/etc/parallelcluster/cfnconfig"
 
 bucket="<bucket>"
